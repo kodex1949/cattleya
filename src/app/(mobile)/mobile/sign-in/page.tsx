9340e536cb2f930301a9e017bc18a3b9f073dc0a
KECCAK <kodex1949@gmail.com>
2026-05-09 23:07:32 +0200

    fix: force dynamic sign in page

--- src/app/(mobile)/mobile/sign-in/page.tsx
+++ src/app/(mobile)/mobile/sign-in/page.tsx
@@ -1,8 +1,12 @@
+export const dynamic = "force-dynamic";
+
+export const revalidate = 0;
+
 import SignInFormMobileCattleya from "@/components/mobile/cattleya/account/SignInFormMobileCattleya";
 
 export default function SignInPage() {
   return (
-    <main className="min-h-screen bg-[#080706]">
+    <main className="min-h-screen bg-[#f7f3ec]">
       <SignInFormMobileCattleya />
     </main>
   );

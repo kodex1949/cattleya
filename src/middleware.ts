9fd15eec09786aad7b0fc7a22dee7981400e2b0d
KECCAK <kodex1949@gmail.com>
2026-05-31 21:29:15 +0200

    Fix root page redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,31 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // Ignore fichiers statiques / API
   if (
+    pathname === "/" ||
     pathname.startsWith("/_next") ||
     pathname.startsWith("/api") ||
+    pathname.startsWith("/mobile") ||
+    pathname.startsWith("/pc") ||
     pathname.includes(".")
   ) {
     return NextResponse.next();
-  }
-
-  const userAgent = request.headers.get("user-agent") || "";
-
-  const isMobile =
-    /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(userAgent);
-
-  const isOnMobileRoute = pathname.startsWith("/mobile");
-  const isOnPcRoute = pathname.startsWith("/pc");
-
-  // 📱 MOBILE → redirige vers /mobile
-  if (isMobile && !isOnMobileRoute) {
-    return NextResponse.redirect(new URL(`/mobile`, request.url));
-  }
-
-  // 💻 PC → redirige vers /pc
-  if (!isMobile && !isOnPcRoute) {
-    return NextResponse.redirect(new URL(`/pc`, request.url));
   }
 
   return NextResponse.next();
